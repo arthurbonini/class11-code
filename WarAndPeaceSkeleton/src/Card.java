d94466108548e209b8c5a08d9bd06a366d4ecb2d
Arthur Bonini <arthurdbonini@gmail.com>
2016-10-02 11:17:20 -0400

    Added constructor, toString, winner, and getValue Methods

--- WarAndPeaceSkeleton/src/Card.java
+++ WarAndPeaceSkeleton/src/Card.java
@@ -1,13 +1,31 @@
 
 public class Card {
+	CardSuit suit;
+	CardValue value;
+	
+	public Card(int cardNum){
+		suit = CardSuit.values()[cardNum / 13];
+		value = CardValue.values()[cardNum % 13];
+	}
 	
 	public String toString() {
-			//TODO: complete this method definition
+		return value + " of " + suit;
+	}
+	public String toString(boolean symbols) {
+		if (symbols) {
+			return value.getSymbol() + suit.getSymbol();
+		} else {
+			return toString();
+		}
 	}
 	
 
 	public boolean winner(Card c) {
-			// TODO: complete this method definition
+		return getValue() > c.getValue();
+	}
+	
+	private double getValue(){
+		return value.ordinal() + (suit.ordinal()/10.0);
 	}
 	
 }
