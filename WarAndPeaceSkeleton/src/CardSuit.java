28e9d6f8fea89dd95e7f30018eac93ecb26b7792
Arthur Bonini <arthurdbonini@gmail.com>
2016-10-02 11:18:00 -0400

    Initial Commit Also added getSymbol Method

--- WarAndPeaceSkeleton/src/CardSuit.java
+++ WarAndPeaceSkeleton/src/CardSuit.java
@@ -1,4 +1,9 @@
 
 public enum CardSuit {
-
+	Clubs, Diamonds, Hearts, Spades;
+	
+	private char symbol[] = {'\u2663', '\u2666', '\u2764', '\u2660'};
+	public char getSymbol(){
+		return symbol[ordinal()];
+	}
 }
