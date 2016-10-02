f7ba2c36f4e15fd525ae09239cffc8de88db9f11
Arthur Bonini <arthurdbonini@gmail.com>
2016-10-02 15:56:06 -0400

    Added Javadoc and comments and added getValue methods.

--- WarAndPeaceSkeleton/src/CardSuit.java
+++ WarAndPeaceSkeleton/src/CardSuit.java
@@ -1,9 +1,48 @@
-
+/**
+ * Suits that can be used in the Card Class
+ * 
+ * @author Arthur Bonini
+ *
+ */
 public enum CardSuit {
-	Clubs, Diamonds, Hearts, Spades;
+	/**
+	 * Clubs (Value of 0.0)
+	 */
+	Clubs, 
 	
+	/**
+	 * Diamonds (Value of 0.1)
+	 */
+	Diamonds, 
+	
+	/**
+	 * Hearts (Value of 0.2)
+	 */
+	Hearts, 
+	
+	/**
+	 * Spades (Value of 0.3)
+	 */
+	Spades;
+	
+	//Unicode symbols for the four suits
 	private char symbol[] = {'\u2663', '\u2666', '\u2764', '\u2660'};
+	
+	/**
+	 * Returns the Unicode character associated with the selected Card Suit
+	 * 
+	 * @return a unicode character
+	 */
 	public char getSymbol(){
 		return symbol[ordinal()];
+	}
+	
+	/**
+	 * Returns the specified value of the Suit
+	 * 
+	 * @return a numeric value of the suit
+	 */
+	public double getValue(){
+		return ordinal()/10.0;
 	}
 }
--- WarAndPeaceSkeleton/src/CardValue.java
+++ WarAndPeaceSkeleton/src/CardValue.java
@@ -1,9 +1,93 @@
-
+/**
+ * Values that can be used for in the Card Class
+ * 
+ * @author Arthur Bonini
+ *
+ */
 public enum CardValue {
-	Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace;
+	/**
+	 * Two (Value of 0)
+	 */
+	Two, 
 	
+	/**
+	 * Three (Value of 1)
+	 */
+	Three, 
+	
+	/**
+	 * Four (Value of 2)
+	 */
+	Four, 
+	
+	/**
+	 * Five (Value of 3)
+	 */
+	Five, 
+	
+	/**
+	 * Six (Value of 4)
+	 */
+	Six, 
+	
+	/**
+	 * Seven (Value of 5)
+	 */
+	Seven, 
+	
+	/**
+	 * Eight (Value of 6)
+	 */
+	Eight, 
+	
+	/**
+	 * Nine (Value of 7)
+	 */
+	Nine, 
+	
+	/**
+	 * Ten (Value of 8)
+	 */
+	Ten, 
+	
+	/**
+	 * Jack (Value of 9)
+	 */
+	Jack, 
+	
+	/**
+	 * Queen (Value of 10)
+	 */
+	Queen, 
+	
+	/**
+	 * King (Value of 11)
+	 */
+	King, 
+	
+	/**
+	 * Ace (Value of 12)
+	 */
+	Ace;
+	
+	//Short string representations of the Card Values
 	private String symbol[] = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+	
+	/**
+	 * Returns a short string representation of the Card Value
+	 * 
+	 * @return a string representation of the Card Value
+	 */
 	public String getSymbol(){
 		return symbol[ordinal()];
+	}
+	
+	/**
+	 * Returns the specified value of the each Card Value
+	 * 
+	 * @return a numeric value of the Card Value
+	 */
+	public double getValue(){
+		return ordinal();
 	}
 }
