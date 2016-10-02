5bd951d38f88540c53243f05ccf6e91986d4af30
Arthur Bonini <arthurdbonini@gmail.com>
2016-10-02 11:18:42 -0400

    Initial Commit Also added getSymbol method

--- WarAndPeaceSkeleton/src/CardValue.java
+++ WarAndPeaceSkeleton/src/CardValue.java
@@ -1,4 +1,9 @@
 
 public enum CardValue {
-
+	Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace;
+	
+	private String symbol[] = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+	public String getSymbol(){
+		return symbol[ordinal()];
+	}
 }
